f8d28cee94591cdb27996a3e9b9835dad8575f14
TCast33 <104533856+TCast33@users.noreply.github.com>
2024-12-02 16:14:45 -0500

    Add files via upload
    Wasn't able to push

---
+++ GoogleAPI.js
@@ -0,0 +1,8 @@
+curl -X POST -d '{
+    "textQuery" : "Taco Bells near $ZIPCODE"
+  }' \
+  -H 'Content-Type: application/json' -H 'X-Goog-Api-Key: AIzaSyBVFY89pfgd00sa4QhQ4n81d8e5H7l5K1Q
+  
+  ' \
+  -H 'X-Goog-FieldMask: places.displayName,places.formattedAddress,places.priceLevel' \
+  'https://places.googleapis.com/v1/places:searchText'
